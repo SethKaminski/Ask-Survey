193fb580865b733f24b5a41e22185a7a8f84b6d6
SethKaminski <sethgabe@yahoo.com>
2017-03-31 18:43:20 -0400

    Removed Unneeded Code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,27 +3,3 @@ module.exports.DisplayHome = (req, res)  => {
         title: 'Home',
         username: req.user ? req.user.username : '' });
 }
-
-module.exports.DisplayAbout = (req, res) => {
-    res.render('content/about', { 
-        title: 'About',
-        username: req.user ? req.user.username : '' });
-}
-
-module.exports.DisplayProjects = (req, res) => {
-    res.render('content/projects', { 
-        title: 'Projects',
-        username: req.user ? req.user.username : '' });
-}
-
-module.exports.DisplayServices = (req, res) => {
-    res.render('content/services', { 
-        title: 'Services',
-        username: req.user ? req.user.username : '' });
-}
-
-module.exports.DisplayContact = (req, res) => {
-    res.render('content/contact', { 
-        title: 'Contact',
-        username: req.user ? req.user.username : '' });
-}
