63a9ed8f58ea98dceb01e1889210f37b3ee858db
SethKaminski <sethgabe@yahoo.com>
2017-04-07 18:09:26 -0400

    working on creat survey

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,3 +3,16 @@ module.exports.DisplayHome = (req, res)  => {
         title: 'Home',
         username: req.user ? req.user.username : '' });
 }
+
+module.exports.DisplayCreat = (req, res)  => {
+    res.render('content/creat-survey', { 
+        title: 'Creat',
+        username: req.user ? req.user.username : '' });
+}
+
+module.exports.ProcessCreat = (req, res)  => {
+    res.render('content/creat-questions', { 
+        title: 'Creat',
+        surveyName: req.body.surveyName,
+        username: req.user ? req.user.username : '' });
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,6 @@
 let express = require('express');
 let router = express.Router();
-
-let passport = require('passport');
-let UserModel = require('../models/users');
-let User = UserModel.User;
-
 let indexController = require('../controllers/index');
-
-//check if authenticated
-function requireAuth(req, res, next) {
-  if(!req.isAuthenticated()) {
-    return res.redirect('auth/login');
-  }
-  next();
-}
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
@@ -25,19 +12,15 @@ router.get('/about', (req, res, next) => {
   indexController.DisplayAbout(req, res);
 });
 
-/* GET Projects page. */
-router.get('/projects', (req, res, next) => {
-  indexController.DisplayProjects(req, res);
-});
+/* GET creat page. */
+router.get('/creat', (req, res, next) => {
+    indexController.DisplayCreat(req, res);
+  }
+);
 
-/* GET Services page. */
-router.get('/services', (req, res, next) => {
-  indexController.DisplayServices(req, res);
-});
-
-/* GET Contact page. */
-router.get('/contact', (req, res, next) => {
-  indexController.DisplayContact(req, res);
-});
+router.post("/creat-questions", (req, res, next) => {
+    indexController.ProcessCreat(req, res);
+  }
+);
 
 module.exports = router;
